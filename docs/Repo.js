ae0f1fa73e6b2d68b681e969d615b5cffa7d4242
shahart <shahart@users.noreply.github.com>
2024-12-15 19:43:09 +0200

    allow gematria when value is known

--- docs/Repo.js
+++ docs/Repo.js
@@ -137,38 +137,41 @@ class Repo {
     }
 
     gim(str) {
-        let sum = 0;
-        const aleph = 1488;
-        for (let i = 0; i < str.length; ++ i) {
-          if (str.charCodeAt(i) >= aleph && str.charCodeAt(i) <= (aleph+10-1)) {
-            sum += str.charCodeAt(i) - (aleph-1);
-          }
-          else if (str.charCodeAt(i) === aleph+10 || str.charCodeAt(i) === aleph+11) {
-            sum += 20;
-          }
-          else if (str.charCodeAt(i) === aleph+12) {
-            sum += 30;
-          }
-          else if (str.charCodeAt(i) === aleph+13 || str.charCodeAt(i) === aleph+14) {
-            sum += 40;
-          }
-          else if (str.charCodeAt(i) === aleph+15 || str.charCodeAt(i) === aleph+16 || str.charCodeAt(i) === 1487) {
-            sum += 50;
-          }
-          else if (str.charCodeAt(i) === aleph+17) {
-            sum += 60;
-          }
-          else if (str.charCodeAt(i) === aleph+18) {
-            sum += 70;
-          }
-          else if (str.charCodeAt(i) === aleph+19 || str.charCodeAt(i) === aleph+20) {
-            sum += 80;
-          }
-          else if (str.charCodeAt(i) === aleph+21 || str.charCodeAt(i) === aleph+22) {
-            sum += 90;
-          }
-          else if (str.charCodeAt(i) >= aleph+23 && str.charCodeAt(i) <= aleph+23+4-1) {
-            sum += 100 * (str.charCodeAt(i) - (aleph+23) + 1);
+        let sum = parseInt(str);
+        if (isNaN(sum)) {
+            sum = 0;
+            const aleph = 1488;
+            for (let i = 0; i < str.length; ++ i) {
+              if (str.charCodeAt(i) >= aleph && str.charCodeAt(i) <= (aleph+10-1)) {
+                sum += str.charCodeAt(i) - (aleph-1);
+              }
+              else if (str.charCodeAt(i) === aleph+10 || str.charCodeAt(i) === aleph+11) {
+                sum += 20;
+              }
+              else if (str.charCodeAt(i) === aleph+12) {
+                sum += 30;
+              }
+              else if (str.charCodeAt(i) === aleph+13 || str.charCodeAt(i) === aleph+14) {
+                sum += 40;
+              }
+              else if (str.charCodeAt(i) === aleph+15 || str.charCodeAt(i) === aleph+16 || str.charCodeAt(i) === 1487) {
+                sum += 50;
+              }
+              else if (str.charCodeAt(i) === aleph+17) {
+                sum += 60;
+              }
+              else if (str.charCodeAt(i) === aleph+18) {
+                sum += 70;
+              }
+              else if (str.charCodeAt(i) === aleph+19 || str.charCodeAt(i) === aleph+20) {
+                sum += 80;
+              }
+              else if (str.charCodeAt(i) === aleph+21 || str.charCodeAt(i) === aleph+22) {
+                sum += 90;
+              }
+              else if (str.charCodeAt(i) >= aleph+23 && str.charCodeAt(i) <= aleph+23+4-1) {
+                sum += 100 * (str.charCodeAt(i) - (aleph+23) + 1);
+              }
           }
       }
       return sum;
