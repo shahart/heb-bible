acea89530b7c016f556013c21b61e7c6e06d771e
shahart <shahart@users.noreply.github.com>
2024-12-17 01:24:23 +0200

    compare between numbers

--- docs/Read.js
+++ docs/Read.js
@@ -15,7 +15,7 @@ class Read {
 
     read(bookPrk) {
         let bookNum = document.getElementById('BookSelect').value;
-        if (bookPrk) bookPrk=bookPrk.split(",")[1];
+        if (bookPrk) bookPrk=Number(bookPrk.split(",")[1]);
         this.output = "";
         let letters = 0;
         let psukim = 0;
@@ -23,7 +23,7 @@ class Read {
         let totLetters = 0;
         for (let i = 0; i < this.repo.getVerses().length; ++i) {
             if (this.repo.getBookNumArr()[i] == bookNum-1 ) {
-                if (!!!bookPrk || this.repo.getPPrk()[i] >= bookPrk) {
+                if (!!!bookPrk || Number(this.repo.getPPrk()[i]) >= bookPrk) {
                     ++ psukim;
                     if (this.repo.getPPsk()[i] == 1 || (this.repo.getBookNumArr()[i] == 27-1 && this.repo.getPPrk()[i] == 119 && this.repo.getPPsk()[i] % 8 == 1)) this.output += "</br>";
                     this.output += this.no2gim.no2gim(this.repo.getPPrk()[i]) + "-" + this.repo.getPPsk()[i] + " -- " + this.repo.getVerses()[i] + "<br/>";
