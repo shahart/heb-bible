143c0a8ba92b29f46f4faf7a3ca763d8d6e3c342
shahart <shahart@users.noreply.github.com>
2024-12-22 23:56:05 +0200

    visualize the fact it's a specific perek

--- docs/Read.js
+++ docs/Read.js
@@ -21,6 +21,9 @@ class Read {
         let psukim = 0;
         let tevot = 0;
         let totLetters = 0;
+        if (bookPrk) {
+            this.output = " פרק " + this.no2gim.no2gim(bookPrk) + "  </br>";
+        }
         for (let i = 0; i < this.repo.getVerses().length; ++i) {
             if (this.repo.getBookNumArr()[i] == bookNum-1 ) {
                 if (!!!bookPrk || Number(this.repo.getPPrk()[i]) >= bookPrk) {
