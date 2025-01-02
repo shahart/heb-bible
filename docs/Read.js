1c2e889f4d457c5c438550b490dc587f3a45de9f
shahart <shahart@users.noreply.github.com>
2025-01-02 23:43:16 +0200

    offline support; english books' name

--- docs/Read.js
+++ docs/Read.js
@@ -16,6 +16,10 @@ class Read {
     read(bookPrk) {
         let bookNum = document.getElementById('BookSelect').value;
         if (bookPrk) bookPrk=Number(bookPrk.split(",")[1]);
+        var bookeng = ['Genesis','Exodus','Leviticus','Numbers','Deuteronomy','Joshua','Judges','Samuel 1',
+            'Samuel 2','Kings 1','Kings 2','Isaiah','Jeremiah','Ezekiel','Hosea','Joel','Amos','Obadiah','Jonah','Micha','Nachum',
+            'Habakkuk','Zephaniah','Haggai','Zechariah','Malachi','Psalms','Proverbs','Job','Song of songs','Ruth','Lamentations',
+            'Ecclesiastes','Esther','Daniel','Ezra','Nehemiah','Cronicles 1','Cronicles 2']; // 39 books
         this.output = "";
         let letters = 0;
         let psukim = 0;
@@ -33,20 +37,23 @@ class Read {
                     ++ psukim;
                     if (this.repo.getPPsk()[i] == 1 || (this.repo.getBookNumArr()[i] == 27-1 && this.repo.getPPrk()[i] == 119 && this.repo.getPPsk()[i] % 8 == 1)) this.output += "</br>";
                     if (this.repo.getPPsk()[i] == 1)
-                        this.output += "<a id=\"prk" + this.repo.getPPrk()[i] + "\"/>";
-                    this.output += this.no2gim.no2gim(this.repo.getPPrk()[i]) + "-" + this.repo.getPPsk()[i] + " -- " + this.repo.getVerses()[i] + "<br/>";
+                        this.output += "<a id=\"prk" + this.repo.getPPrk()[i] + "\"/>" + "<a href=\"#book\">";
+                    this.output += this.no2gim.no2gim(this.repo.getPPrk()[i]);
+                    if (this.repo.getPPsk()[i] == 1)
+                        this.output += "</a>";
+                    this.output += "-" + this.repo.getPPsk()[i] + " -- " + this.repo.getVerses()[i] + "<br/>";
                     totLetters += this.repo.getVerses()[i].replace(/\s+/g, '').length;
                     tevot += this.repo.getVerses()[i].split(' ').length; // - 1;
                     if (bookPrk)
                         this.output += "</span>";
                     if (this.repo.getPPsk()[i] == 1) {
-                        headers += "<a href=\"#prk" + this.repo.getPPrk()[i] + "\">" + this.no2gim.no2gim(this.repo.getPPrk()[i]) + "</a> ";
+                        headers += "<a id=\"book\"/><a href=\"#prk" + this.repo.getPPrk()[i] + "\">" + this.no2gim.no2gim(this.repo.getPPrk()[i]) + "</a> ";
                     }
                 }
             }
         }
         if (!!!bookPrk) {
-            this.output = "פסוקים: " + psukim + "</br>" + "אותיות: " + totLetters + "</br>" + "תיבות: " + tevot + "</br></br>" + headers + "</br>" + this.output;
+            this.output = bookeng[bookNum-1] + "</br></br>" + "פסוקים: " + psukim + "</br>" + "אותיות: " + totLetters + "</br>" + "תיבות: " + tevot + "</br></br>" + headers + "</br>" + this.output;
         }
         document.getElementById("bibleResult").innerHTML = this.output;
     }
