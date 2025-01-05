bc641a44643e0da5ecc79d05b47500c707a426b2
shahart <shahart@users.noreply.github.com>
2025-01-05 18:40:45 +0200

    Kickoff Rashi & English urls

--- docs/Read.js
+++ docs/Read.js
@@ -15,6 +15,13 @@ class Read {
 
     read(bookPrk) {
         let bookNum = document.getElementById('BookSelect').value;
+        let bookRashiNum = parseInt(bookNum);
+        if (bookRashiNum >= 15) bookRashiNum += 1;
+        if (bookRashiNum >= 28) bookRashiNum += 1;
+        if (bookRashiNum >= 30) bookRashiNum += 1;
+        if (bookRashiNum == 32) bookRashiNum = 31; // todo better fix
+        let bookHeb = document.getElementById('BookSelect').options[bookRashiNum-1].text.replace(' ', '_');
+        if (bookHeb === 'תהלים') bookHeb = 'תהילים';
         if (bookPrk) bookPrk=Number(bookPrk.split(",")[1]);
         var bookeng = ['Genesis','Exodus','Leviticus','Numbers','Deuteronomy','Joshua','Judges','Samuel 1',
             'Samuel 2','Kings 1','Kings 2','Isaiah','Jeremiah','Ezekiel','Hosea','Joel','Amos','Obadiah','Jonah','Micha','Nachum',
@@ -41,7 +48,8 @@ class Read {
                     this.output += this.no2gim.no2gim(this.repo.getPPrk()[i]);
                     if (this.repo.getPPsk()[i] == 1)
                         this.output += "</a>";
-                    this.output += "-" + this.repo.getPPsk()[i] + " -- " + this.repo.getVerses()[i] + "<br/>";
+                    let rashiUrl = "<a href=\"" + "https://wiki.jewishbooks.org.il/mediawiki/wiki/%D7%A8%D7%A9%22%D7%99/" + bookHeb + "/" + this.no2gim.no2gim(this.repo.getPPrk()[i]) + "#" + this.no2gim.no2gim(this.repo.getPPsk()[i]) + "\"" + " target=\"_blank\">" + this.repo.getPPsk()[i] + "</a>";
+                    this.output += "-" + rashiUrl + " -- " + this.repo.getVerses()[i] + "<br/>";
                     totLetters += this.repo.getVerses()[i].replace(/\s+/g, '').length;
                     tevot += this.repo.getVerses()[i].split(' ').length; // - 1;
                     if (bookPrk)
@@ -53,7 +61,18 @@ class Read {
             }
         }
         if (!!!bookPrk) {
-            this.output = bookeng[bookNum-1] + "</br></br>" + "פסוקים: " + psukim + "</br>" + "אותיות: " + totLetters + "</br>" + "תיבות: " + tevot + "</br></br>" + headers + "</br>" + this.output;
+            let pointer = bookRashiNum;
+            if (pointer < 10) pointer = '0' + pointer;
+            if (pointer == '08') pointer += 'a';
+            if (pointer == '09') pointer = '08b';
+            if (pointer == '10') pointer = '09a';
+            if (pointer == '11') pointer = '09b';
+            if (pointer >= 12) pointer -= 2;
+            if (pointer >= 14) pointer -= 1;
+            if (pointer == 29) pointer = 27; // אמת
+            // todo fine-tune the כתובים ..
+            let engTrans = "<a href=\"https://mechon-mamre.org/p/pt/pt" + pointer + "01.htm\" target=\"_blank\">" + bookeng[bookNum-1] + "</a>";
+            this.output = engTrans + "</br></br>" + "פסוקים: " + psukim + "</br>" + "אותיות: " + totLetters + "</br>" + "תיבות: " + tevot + "</br></br>" + headers + "</br>" + this.output;
         }
         document.getElementById("bibleResult").innerHTML = this.output;
     }
