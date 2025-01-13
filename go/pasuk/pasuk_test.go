ce50ac9922ef0416ccd573f07786103c2b2a3bd8
shahart <shahart@users.noreply.github.com>
2025-01-13 19:54:53 +0200

    target=_new when openning refs

--- go/pasuk/pasuk_test.go
+++ go/pasuk/pasuk_test.go
@@ -2,10 +2,21 @@ package pasuk_test
 
 import (
 	"example.com/pasuk"
-	"fmt"
+	"testing"
 )
 
-func ExampleShahar() {
-	fmt.Println(pasuk.Pasuk("שחר", false))
-	// Output: 25
+func TestPasuk(t *testing.T) {
+	for _, c := range []struct {
+		in           string
+		containsName bool
+		want         int
+	}{
+		{"שחר", false, 25},
+		{"שחר", true, 75},
+	} {
+		got := pasuk.Pasuk(c.in, c.containsName)
+		if got != c.want {
+			t.Errorf("%q == %d, want %d", c.in, got, c.want)
+		}
+	}
 }
