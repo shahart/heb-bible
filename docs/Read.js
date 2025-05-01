4c5d2aa0308554ff4804b4fdc9a4c5c4bd193214
shahart <shahart@users.noreply.github.com>
2025-05-01 14:23:19 +0300

    Field demand - count tevot per perek

--- docs/Read.js
+++ docs/Read.js
@@ -48,6 +48,10 @@ class Read {
         let letters = 0;
         let psukim = 0;
         let tevot = 0;
+        let prkTevot = 0;
+        let prevTevot = 0;
+        let prkLetters = 0;
+        let prevLetters = 0;
         let totLetters = 0;
         let headers = "";
         if (bookPrk && !!!bookPsk) {
@@ -56,8 +60,11 @@ class Read {
         if (bookNum == 27 && !!!bookPrk) {
             this.output += "</br>ליום ראשון - א, ליום שני - <a id=\"book\"/><a href=\"#prk30\">ל</a> , ליום שלישי - <a id=\"book\"/><a href=\"#prk51\">נא</a> , ליום רביעי - <a id=\"book\"/><a href=\"#prk73\">עג</a> , ליום חמישי - <a id=\"book\"/><a href=\"#prk90\">צ</a> , ליום שישי - <a id=\"book\"/><a href=\"#prk107\">קז</a> , ליום השבת - <a id=\"book\"/><a href=\"#prk120\">קכ</a>  " + "</br>";
         }
+        // console.info("Book "+ bookNum);
         // todo output += בראשית נח לך-לך וירא and maybe with pointer
+        var lastI = 0;
         for (let i = 0; i < this.repo.getVerses().length; ++i) {
+            lastI = i;
             if (this.repo.getBookNumArr()[i] == bookNum-1 ) {
                 if (!!!bookPrk || Number(this.repo.getPPrk()[i]) == bookPrk) {
                     if (bookPrk)
@@ -67,44 +74,66 @@ class Read {
                         if (this.repo.getPPsk()[i] == 1 || (this.repo.getBookNumArr()[i] == 27-1 && this.repo.getPPrk()[i] == 119 && this.repo.getPPsk()[i] % 8 == 1)) 
                             this.output += "</br>";
                         // todo if psk,prk,book in Parashot, + br
-                        if (this.repo.getPPsk()[i] == 1)
-                            this.output += "<a id=\"prk" + this.repo.getPPrk()[i] + "\"/>" + "<a href=\"#book\">";
+                        if (this.repo.getPPsk()[i] == 1) {
+                            if (this.repo.getPPrk()[i] >= 2) {
+                                this.output += "<a id=\"prk" + this.repo.getPPrk()[i] + "\"/>" + "<a onClick=\"alert(' פרק ' + " + (this.repo.getPPrk()[i]-1) + " + ' תיבות ' + " + (prkTevot - prevTevot)  + " + ' אותיות ' + " + (prkLetters  - prevLetters) + ")\" href=\"#book\">";
+                            }
+                            else {
+                                this.output += "<a id=\"prk" + this.repo.getPPrk()[i] + "\"/>" + "<a href=\"#book\">";
+                            }
+                        }
                         this.output += this.no2gim.no2gim(this.repo.getPPrk()[i]);
                         if (this.repo.getPPsk()[i] == 1)
                             this.output += "</a>";
                         let rashiUrl = "<a href=\"" + "https://wiki.jewishbooks.org.il/mediawiki/wiki/%D7%A8%D7%A9%22%D7%99/" + bookHeb + "/" + this.no2gim.no2gim(this.repo.getPPrk()[i]) + "#" + this.no2gim.no2gim(this.repo.getPPsk()[i]) + "\"" + " target=\"_new\">" + this.repo.getPPsk()[i] + "</a>";
                         this.output += "-" + rashiUrl + " -- ";
                         if (this.repo.getNikkudVerses()[i].length >= 1) { 
-                            this.output += this.repo.getNikkudVerses()[i] + "<br/>";
+                            this.output += this.repo.getNikkudVerses()[i];
                         }
                         else {
-                            this.output += this.repo.noName(this.repo.getVerses()[i]) + "<br/>";
+                            this.output += this.repo.noName(this.repo.getVerses()[i]);
                         }
+                        this.output += "<br/>";
                         totLetters += this.repo.getVerses()[i].replace(/\s+/g, '').length;
                         tevot += this.repo.getVerses()[i].split(' ').length; // - 1;
                         if (bookPrk)
                             this.output += "</span>";
                         if (this.repo.getPPsk()[i] == 1) {
+                            if (this.repo.getPPrk()[i] > 1) {
+                                // headers += " " +  (tevot - prevTevot);
+                                // console.info("Perek " + this.repo.getPPrk()[i-1] + " Tevot " + (prkTevot - prevTevot) + " Letters: " +  (prkLetters - prevLetters));
+                            }
                             headers += "<a id=\"book\"/><a href=\"#prk" + this.repo.getPPrk()[i] + "\">" + this.no2gim.no2gim(this.repo.getPPrk()[i]) + "</a> ";
+                            prevTevot = prkTevot;
+                            prevLetters = prkLetters;
                         }
+                        prkLetters += this.repo.getVerses()[i].replace(/\s+/g, '').length;
+                        prkTevot += this.repo.getVerses()[i].split(' ').length; // - 1;
                         clearText += this.repo.getVerses()[i] + ": ";
                     }
                 }
             }
             else if (this.repo.getBookNumArr()[i] > bookNum-1 ) {
                 if (bookeng[bookNum-1] === 'Isaiah')
-                    this.output += "<br/>" + this.repo.getVerses()[i-2];
+                    this.output += "<br/>" + this.repo.getVerses()[i-2] + "<br/>";
                 else if (bookeng[bookNum-1] === 'Malachi')
-                    this.output += "<br/>" + this.repo.getVerses()[i-2];
+                    this.output += "<br/>" + this.repo.getVerses()[i-2] + "<br/>";
                 else if (bookeng[bookNum-1] === 'Song of songs')
-                    this.output += "<br/>" + this.repo.getVerses()[i-2];
+                    this.output += "<br/>" + this.repo.getVerses()[i-2] + "<br/>";
                 else if (bookeng[bookNum-1] === 'Lamentations')
-                    this.output += "<br/>" + this.repo.getVerses()[i-2];
+                    this.output += "<br/>" + this.repo.getVerses()[i-2] + "<br/>";
                 else if (bookeng[bookNum-1] === 'Ecclesiastes')
-                    this.output += "<br/>" + this.repo.getVerses()[i-2];
+                    this.output += "<br/>" + this.repo.getVerses()[i-2] + "<br/>";
                 break;
             }
         }
+
+        this.output = this.output.substring(0, this.output.length - "<br/>".length);
+        this.output += "<a id=\"lastPrk" + "\"/>" + "<a onClick=\"alert(' פרק אחרון תיבות ' + " + (prkTevot - prevTevot)  + " + ' אותיות ' + " + (prkLetters  - prevLetters) + ")\" href=\"#book\">.</a>";
+        
+        // console.info("Perek " + this.repo.getPPrk()[lastI-1] + " Tevot " + (prkTevot - prevTevot) + " Letters: " +  (prkLetters - prevLetters));
+        // console.info("Tevot on last chapter " + (prevTevot - tevot));
+        
         if (!!!bookPrk) {
             var pointerArr = ['01','02','03','04','05','06','07','08a',
                 '08b','09a','09b','10','11','12','13','14','15','16','17','18','19',
