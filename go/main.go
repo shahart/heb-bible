3ff44a60f7c372e86aedc3524e1c25508a1516ba
shahart <shahart@users.noreply.github.com>
2026-06-21 00:47:54 +0300

    Go. Serve /psukim over HTTP and delegate to pasuk.Pasuk

--- go/main.go
+++ go/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"example.com/pasuk"
-	"fmt"
-	"os"
+	"encoding/json"
+	"log"
+	"net/http"
 	"strconv"
+
+	"example.com/pasuk"
 )
 
-func main() {
-	name := os.Args[1]
-	containsName := false
-	if len(os.Args) > 2 {
-		containsName, _ = strconv.ParseBool(os.Args[2])
-		// if _ != nil {
-		// fmt.Println("Ignoring 2nd param containsName:", err)
-		// }
+type response struct {
+	Count int `json:"count"`
+}
+
+func psukimHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		http.Error(w, "missing 'name' query parameter", http.StatusBadRequest)
+		return
 	}
-	fmt.Println(pasuk.Pasuk(name, containsName), "verses found")
+	containsName := false
+	if v := r.URL.Query().Get("containsName"); v != "" {
+		b, err := strconv.ParseBool(v)
+		if err == nil {
+			containsName = b
+		}
+	}
+
+	count := pasuk.Pasuk(name, containsName)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(response{Count: count}); err != nil {
+		log.Println("encode response:", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/psukim", psukimHandler)
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
