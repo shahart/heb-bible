aaa44c0273495914b0a2cd5ef88a8437908b0945
shahart <shahart@users.noreply.github.com>
2026-07-10 11:46:43 +0300

    sb4. swagger test

--- sb3/src/test/java/edu/hebbible/HebBibleTest.java
+++ sb3/src/test/java/edu/hebbible/HebBibleTest.java
@@ -3,15 +3,25 @@ package edu.hebbible;
 //import io.awspring.cloud.dynamodb.DynamoDbTemplate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 //import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HebBibleTest {
 
 //    @MockBean
 //    DynamoDbTemplate dynamoDbTemplate;
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     void contextLoads(ApplicationContext applicationContext) {
@@ -19,5 +29,13 @@ class HebBibleTest {
         Assertions.assertTrue(applicationContext.getBeanDefinitionCount() > 0);
     }
 
-}
+    @Test
+    void swaggerUiIsPublic() throws Exception {
+        mvc.perform(get("/swagger-ui/index.html"))
+                .andExpect(status().isOk());
 
+        mvc.perform(get("/v3/api-docs"))
+                .andExpect(status().isOk());
+    }
+
+}
